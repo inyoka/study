b9d909a807d147735f9de2dcca1af994e4782883
inyoka <simonmarkholland@gmail.com>
2017-01-23 06:38:02 +0000

    Split views into sep module

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+# Entry point for Student Database
+
+from flask import Flask, request, url_for
 
 app = Flask(__name__)
+app.secret_key = 'This is really unique and secret'
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import url_for, request
+import random
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hellow, World!"
+def hello_person():
+    return """
+        <h1>CaSE Online Database Development</h1>
+        <p>Hello Iain!  Please check back here periodically for developments.  Much </p>
+        <p>of the work will be in the background, but I will let you know when things </p>
+        <p>start to change on here.</p>
+        <hr>
+        <p>Test sending, processing and recieving data...</p>
+        <p>Who do you want me to say "Hi" to with a random greating?</p>
+        <form method="POST" action="%s"><input name="person" /><input type="submit" value="Go!" /></form>
+        """ % (url_for('greet'),)
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    greeting = random.choice(["Hiya", "Hallo", "Hola", "Ola", "Salut", "Privet", "Konnichiwa", "Ni hao"])
+    return """
+        <p>%s, %s!</p>
+        <p><a href="%s">Back to start</a></p>
+        """ % (greeting, request.form["person"], url_for('hello_person'))
