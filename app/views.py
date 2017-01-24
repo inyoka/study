7cd844e88ed1d5c91f1774a6f3228f008dba5049
inyoka <simonmarkholland@gmail.com>
2017-01-24 09:35:52 +0700

    Added navigation bar

--- app/views.py
+++ app/views.py
@@ -1,38 +1,30 @@
 from app import app
-from flask import url_for, request
+from flask import url_for, request, render_template
 import random
+
 
 @app.route('/')
 @app.route('/index')
 def hello_person():
-    return """
-        <h1>CaSE Online Database Development</h1>
-        <p>Hello Iain!  Please check back here periodically for developments.  Much </p>
-        <p>of the work will be in the background, but I will let you know when things </p>
-        <p>start to change on here.</p>
-        <hr>
-        <p>Test sending, processing and recieving data...</p>
-        <p>Who do you want me to say "Hi" to with a random greating?</p>
-        <form method="POST" action="%s"><input name="person" /><input type="submit" value="Go!" /></form>
-        """ % (url_for('greet'),)
+    user = {'nickname': 'Iain'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'Iain'},
+            'body': 'Beautiful day in CaSE 2!'
+        },
+        {
+            'author': {'nickname': 'Simon'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user)
+
 
 @app.route('/greet', methods=['POST'])
 def greet():
-    greeting = random.choice(["Hiya", "Hallo", "Hola", "Ola", "Salut", "Privet", "Konnichiwa", "Ni hao"])
+    greeting = random.choice(["Hiya", "Hallo", "Hola", "Ola", "Salut", "Privet",
+                              "Konnichiwa", "Ni hao"])
     return """
         <p>%s, %s!</p>
         <p><a href="%s">Back to start</a></p>
         """ % (greeting, request.form["person"], url_for('hello_person'))
-@app.route('/test')
-def test():
-    user = {'nickname': 'Miguel'}  # fake user
-    return '''
-        <html>
-        <head>
-        <title>Home Page</title>
-        </head>
-        <body>
-        <h1>Hello, ''' + user['nickname'] + '''</h1>
-        </body>
-        </html>
-        '''
