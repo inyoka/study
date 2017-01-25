a4a060d2ae6d1c1134df115ac15ff461edf236e6
inyoka <simonmarkholland@gmail.com>
2017-01-25 14:37:12 +0700

    Final OpenID changes

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,18 @@ import os
 from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir
-
-lm = LoginManager()
-lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
 # END OpenID Implementation
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 # app.secret_key = 'This is really unique and secret'
+
+# OpenID Implementation
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+# END OpenID Implementation
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,16 +1,40 @@
 from app import app, db, lm, oid
-from flask import url_for, request, render_template, flash, redirect, session, url_for, g
+from flask import url_for, request, render_template, flash, redirect, session, g
 from flask_login import login_user, logout_user, current_user, login_required
 import random
 from app.forms import LoginForm
 from app.models import User
 
 
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
 @app.route('/')
 @app.route('/index')
-def hello_person():
-    user = {'nickname': 'Iain'}  # fake user
-    return render_template('index.html', title='Home', user=user)
+@login_required
+def index():
+    user = g.user
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -27,26 +51,32 @@ def login():
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
-@app.route('/greet')
-def greeting():
-    user = {'nickname': 'Iain'}  # fake user
-    posts = [  # fake array of posts
-             {
-                 'author': {'nickname': 'Iain'},
-                 'body': 'Beautiful day in CaSE 2!'
-             },
-             {
-                 'author': {'nickname': 'Simon'},
-                 'body': 'The Avengers movie was so cool!'
-             }
-             ]
-    return render_template('test.html', title='Test', user=user)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 @app.route('/greet', methods=['POST'])
 def greet():
