fc21719bb6a75906fe88c0ed5f5731e8d6732e6c
inyoka <simonmarkholland@gmail.com>
2017-01-25 10:57:42 +0700

    Add conf for using sqlite db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 # Entry point for Student Database
 
-from flask import Flask, url_for
+from flask import Flask #, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 # app.secret_key = 'This is really unique and secret'
 
-from app import views
+from app import views, models
