f215fa4875996781247cc428e46b3ee9d37d4de9
inyoka <simonmarkholland@gmail.com>
2017-01-26 17:53:33 +0700

    Final profile edit changes

--- app/views.py
+++ app/views.py
@@ -92,6 +92,16 @@ def login():
                            providers=app.config['OPENID_PROVIDERS'])
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
 
 @oid.after_login
 def after_login(resp):
