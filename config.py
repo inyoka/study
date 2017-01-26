910edf6094d1d432a3a70ee7ca229e2ff56abd47
inyoka <simonmarkholland@gmail.com>
2017-01-26 09:04:08 +0700

    Fixed login bug, created logout bug

--- config.py
+++ config.py
@@ -8,10 +8,15 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+# BEGIN OAUTH
 
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+GOOGLE_LOGIN_CLIENT_ID = "<your-id-ending-with>.apps.googleusercontent.com"
+GOOGLE_LOGIN_CLIENT_SECRET = "<your-secret>"
+
+OAUTH_CREDENTIALS = {
+    'google': {
+        'id': GOOGLE_LOGIN_CLIENT_ID,
+        'secret': GOOGLE_LOGIN_CLIENT_SECRET
+    }
+}
+# END OAUTH
