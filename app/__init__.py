61f336d3b06b2b4488175905d8f75ef8b6258f63
inyoka <simonmarkholland@gmail.com>
2017-01-29 04:14:45 +0000

    Fixed errors on PythonAnywhere

--- app/__init__.py
+++ app/__init__.py
@@ -7,22 +7,22 @@ from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 # app.secret_key = 'This is really unique and secret'
 
 # OpenID Implementation
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+oid = OpenID(app, os.path.join(basedir, 'mysite/tmp'))
 # END OpenID Implementation
 
+db = SQLAlchemy(app)
 from app import views, models
 
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/study.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('mysite/tmp/study.log', 'a', 1 * 1024 * 1024, 10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
