1fbdbd776acf2161a33800794a2d05b354afe155
inyoka <simonmarkholland@gmail.com>
2017-02-10 16:09:59 +0700

    Improved AddStudent Form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import Form
-
-from wtforms.fields import StringField, BooleanField, TextAreaField, DateField, SelectField, TextAreaField, RadioField, IntegerField, SubmitField
+from wtforms.fields import StringField, BooleanField, FormField, FieldList, TextAreaField, DateField, SelectField, TextAreaField, RadioField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Length
 from app.models import User, Student, Contact
+from app import lookup
 
 
 class LoginForm(Form):
@@ -30,27 +30,29 @@ class EditForm(Form):
             return False
         return True
 
+class ContactForm(Form):
+    id = Student.id
+    mobile_phone = StringField('Number')
+    e_mail = StringField('E-mail')
+
 
 class AddStudent(Form):
     name = StringField('Name :', validators=[DataRequired(), Length(min=4, max=80)])
     address = TextAreaField('Address :', validators=[DataRequired()])
     dob = DateField('Date of Birth :', validators=[DataRequired()])
-    gender = RadioField('Male?', choices=[('value','Male'),('value_two','Female')])
-    goal = SelectField(' :', validators=[DataRequired()])  # Qualification etc
-    target =  SelectField(' :', validators=[DataRequired()]) # Skill needing improvment
-    occupation =  SelectField(' :', validators=[DataRequired()]) # Current occupation
-    status = SelectField(' :', validators=[DataRequired()]) # Active Inactive
-    days = RadioField(' :', validators=[DataRequired()])  # 7 digit binary?
-    time = IntegerField(' :', validators=[DataRequired()]) # Avail after %%:%% on weekday
-    dateEnroll = DateField(' :', validators=[DataRequired()])
-    dateLastContact = DateField(' :', validators=[DataRequired()])
-    lapsedWhy = SelectField(' :', validators=[DataRequired()])
+    gender = RadioField('Male?', choices=[('0','Male'),('1','Female')])
+    contacts = FieldList(FormField(ContactForm))
+    goal = SelectField('Goal :', choices=lookup.GOAL, validators=[DataRequired()])  # Qualification etc
+    target =  SelectField('Target  :', choices=lookup.TARGET, validators=[DataRequired()]) # Skill needing improvment
+    occupation =  SelectField('Occupation :', validators=[DataRequired()]) # Current occupation
+    status = SelectField('Student status :', validators=[DataRequired()]) # Active Inactive
+    days = RadioField('Days available :', choices=lookup.DAYS, validators=[DataRequired()])  # 7 digit binary?
+    time = IntegerField('Available from :', validators=[DataRequired()]) # Avail after %%:%% on weekday
+    dateEnroll = DateField('Date enrolled :', format='%Y-%m-%d', validators=[DataRequired()])
+    dateLastContact = DateField('Last contact :', format='%Y-%m-%d', validators=[DataRequired()])
+    lapsedWhy = SelectField('Why they left :')
     notes = TextAreaField('Notes :', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
-
-
-
 
 
 '''
