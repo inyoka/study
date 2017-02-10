4cdc602d77f2d28a3149cf44fcea86b6fa9b721c
inyoka <simonmarkholland@gmail.com>
2017-02-10 15:55:31 +0700

    Static data structure file added

---
+++ app/lookup.py
@@ -0,0 +1,26 @@
+
+TARGET = [('none', 'None'),
+    ('spoken', 'Spoken'),
+    ('grammar','Grammar'),
+    ('reading','Reading'),
+    ('listening','Listening'),
+    ('writing','Writing'),
+    ('exam','Exam'),
+    ('conversation','Conversation')]
+
+GOAL = [('none', 'None'),
+    ('TOEFL', 'TOEFL'),
+    ('IELTS','IELTS'),
+    ('iGCSE','iGCSE'),
+    ('A-Levels','A-Levels'),
+    ('Professional','Professional Development'),
+    ('Personal','Personal Development'),
+    ('Overseas','Overseas Study')]
+
+DAYS = [('Sunday', 'Sunday'),
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday')]
