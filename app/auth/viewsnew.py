839311cf7e0b4684745cf5f34c70d613045ca056
inyoka <simonmarkholland@gmail.com>
2017-02-16 13:46:19 +0700

    Minor code tidying

--- app/auth/viewsnew.py
+++ app/auth/viewsnew.py
@@ -1,6 +1,7 @@
 # app/auth/views.py
-from flask import url_for, render_template, flash, redirect
+from flask import  flash, redirect, render_template, url_for
 from flask_login import login_required, login_user, logout_user
+
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from .. import db
