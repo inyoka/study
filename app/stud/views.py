2592ad0aafed6e8966de9227f36d2a73e4f6f538
inyoka <simonmarkholland@gmail.com>
2017-02-21 08:11:04 +0700

    Added missing call to flask.request

--- app/stud/views.py
+++ app/stud/views.py
@@ -1,5 +1,5 @@
 from app import db
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_required
 from . import stud
 from .forms import AddStudent, ViewStudent
