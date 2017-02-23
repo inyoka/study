f06a130937caf7c52b866d1548ba366efa380a83
inyoka <simonmarkholland@gmail.com>
2017-02-23 16:13:19 +0700

    Remove validators

--- app/stud/forms.py
+++ app/stud/forms.py
@@ -4,8 +4,10 @@ from wtforms.fields import FieldList, DateField, SelectField, TextAreaField
 from wtforms.fields import RadioField, IntegerField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length
 import wtforms.validators as validators
-from app.models import Student
+from .models import Student
 from app import lookup
+from flask_wtf import Form
+from wtforms.widgets import TextArea
 
 
 class ContactForm(FlaskForm):
@@ -16,9 +18,9 @@ class ContactForm(FlaskForm):
 
 class AddStudent(FlaskForm):
     id = Student.id
-    fullname = StringField('Name :', validators=[DataRequired(), Length(min=4, max=80)])
-    address = TextAreaField('Address :', validators=[DataRequired()])
-    dob = DateField('Date of Birth :', validators=[DataRequired()])
+    fullname = StringField('Name :', validators=[Length(min=4, max=80)])
+    address = TextAreaField('Address :')
+    dob = DateField('Date of Birth :')
     gender = SelectField('Gender', choices=lookup.GENDER, validators=[validators.optional()])
     # contacts = FieldList(FormField(ContactForm))
     goal = SelectField('Goal :', choices=lookup.GOAL)  # Qualification etc
@@ -26,8 +28,8 @@ class AddStudent(FlaskForm):
     occupation = SelectField('Occupation :', validators=[DataRequired()])  # Current occupation
     status = SelectField('Student status :', choices=lookup.STATUS, validators=[DataRequired()])  # Active Inactive
     days = SelectMultipleField('Days available :', choices=lookup.DAYS, validators=[DataRequired()])  # 7 digit binary?
-    time = IntegerField('Available from :', validators=[DataRequired("Enter time available or 00:00 for any.")])  # Avail after %%:%% on weekday
-    dateEnroll = DateField('Date enrolled :', format='%Y-%m-%d', validators=[DataRequired()])
+    time = IntegerField('Available from :')  # Avail after %%:%% on weekday
+    dateEnroll = DateField('Date enrolled :', format='%Y-%m-%d')
     dateLastContact = DateField('Last contact :', format='%Y-%m-%d')
     lapsedWhy = SelectField('Why they left :', choices=lookup.LAPSED)
     notes = TextAreaField('Notes :')
