4e4e1c843a969055473e524e78e6bd6e0db6a73b
inyoka <simonmarkholland@gmail.com>
2017-02-27 12:29:36 +0700

    format for pep8

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,19 +1,24 @@
 # app/auth/forms.py
 
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, ValidationError, TextField, BooleanField
-from wtforms.validators import InputRequired, Email, EqualTo, Length, DataRequired, Length
-from wtforms.fields import SelectMultipleField, FormField, FieldList, DateField, SelectField, TextAreaField, RadioField, IntegerField
+from wtforms import PasswordField, StringField, SubmitField, ValidationError,\
+    TextField, BooleanField, TextAreaField
+from wtforms.validators import InputRequired, Email, EqualTo, Length,\
+    DataRequired
 from ..models import User
 
+
 class RegistrationForm(FlaskForm):
-    """
-    FlaskForm for users to create new account
-    """
-    email = StringField('Email', validators=[InputRequired(), Email(), Length(min=6, max=35)])
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=25)])
-    fullname = StringField('Name', validators=[InputRequired(), Length(min=3, max=40)])
-    password = PasswordField('Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    email = StringField('Email', validators=[InputRequired(), Email(),
+                                             Length(min=6, max=35)])
+    username = StringField('Username', validators=[InputRequired(),
+                                                   Length(min=4, max=25)])
+    fullname = StringField('Name', validators=[InputRequired(),
+                                               Length(min=3, max=40)])
+    password = PasswordField('Password',
+                             validators=[InputRequired(),
+                             EqualTo('confirm',
+                             message='Passwords must match')])
     confirm = PasswordField('Confirm Password')
     submit = SubmitField('Register')
 
@@ -25,12 +30,12 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username is already in use.')
 
+
 class LoginForm(FlaskForm):
     username = TextField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('remember_me', default=False)
     submit = SubmitField('Login')
-
 
     def __init__(self, *args, **kwargs):
         FlaskForm.__init__(self, *args, **kwargs)
@@ -47,33 +52,14 @@ class LoginForm(FlaskForm):
             self.username.errors.append('Unknown username')
             return False
 
-        if not user.check_password(self.password.data):
+        if not self.user.check_password(self.password.data):
             self.password.errors.append('Invalid password')
-            return False
-
-        if not self.user.active:
-            self.username.errors.append(gettext('User not activated'))
             return False
 
         return True
 
 
 class EditForm(FlaskForm):
-    username = StringField('User.username', validators = [DataRequired()])
-    about_me = TextAreaField('User.about_me', validators = [Length(min=0, max=140)])
-
-    def __init__(self, original_username, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
-        self.original_username = original_username
-
-    def validate(self):
-        if not Form.validate(self): #Normal flask's built in check.
-            return False
-        if self.username.data == self.original_username: #If it's just the same, no worries
-            return True
-        user = User.query.filter_by(username=self.username.data).first() #Check database now for
-        #the username guy wants
-        if user != None: #If result returns non-none, already in use.
-            self.username.errors.append('This username is already in use. Please choose another one.')
-            return False
-        return True
+    username = StringField('User.username', validators=[DataRequired()])
+    about_me = TextAreaField('User.about_me',
+                             validators=[Length(min=0, max=140)])
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,11 @@
 # app/auth/old-views.py
-from app import db, lm
-from flask import url_for, request, render_template, flash, redirect, g
-from flask_login import login_user, logout_user, current_user, login_required
+from app import db
+from flask import url_for, request, render_template, flash, redirect
+from flask_login import login_user, logout_user, login_required
 from .forms import LoginForm, RegistrationForm
 from app.models import User
-from datetime import datetime
 from . import auth
 import sqlite3 as sql
-
-
-
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -17,21 +13,16 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data,
-                            username=form.username.data,
-                            fullname=form.fullname.data,
-                            password=form.password.data)
+                    username=form.username.data,
+                    fullname=form.fullname.data,
+                    password=form.password.data)
 
-        # add employee to the database
         db.session.add(user)
         db.session.commit()
         flash('You have successfully registered! You may now login.')
 
-        # redirect to the login page
         return redirect(url_for('auth.login'))
-
-    # load registration template
     return render_template('auth/register.html', form=form, title='Register')
-
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -41,7 +32,6 @@ def login():
     if request.method == 'GET':
         return render_template('auth/login.html', title="login", form=form)
     user = request.form['username']
-    password = request.form['password']
     registered_user = User.query.filter_by(username=user).first()
     if registered_user is None:
         flash('Username is invalid', 'error')
@@ -49,22 +39,7 @@ def login():
     login_user(registered_user, form.remember_me.data)
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('home.index'))
-    '''
 
-    form = LoginForm()
-    if form.validate_on_submit():
-        login_user(user)
-        session['remember_me'] = form.remember_me.data  # Does user want remembering?
-
-        flask.flash('Logged in successfully.')
-
-        next = flask.request.args.get('next')
-        if not is_safe_url(next):
-            return flask.abort(400)
-
-        return flask.redirect(next or flask.url_for('home.index'))
-    return render_template('auth/login.html', title='Sign-In', form=form)
-    '''
 
 @auth.route('/logout')
 def logout():
@@ -83,4 +58,5 @@ def list():
     cur.execute("SELECT * from users ORDER BY id")
 
     rows = cur.fetchall()
-    return render_template("/list.html",title="List Authenticated Users", keys=keys, rows=rows)
+    return render_template("/list.html", title="List Authenticated Users",
+                           keys=keys, rows=rows)
