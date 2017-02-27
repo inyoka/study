706a006c77f7917901911dbfb8ec0f52510b9652
inyoka <simonmarkholland@gmail.com>
2017-02-27 13:27:51 +0700

    Revert to scotch.io login

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,18 +27,24 @@ def register():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm()
-    if request.method == 'GET':
-        return render_template('auth/login.html', title="login", form=form)
-    user = request.form['username']
-    registered_user = User.query.filter_by(username=user).first()
-    if registered_user is None:
-        flash('Username is invalid', 'error')
-        return redirect(url_for('auth.login'))
-    login_user(registered_user, form.remember_me.data)
-    flash('Logged in successfully')
-    return redirect(request.args.get('next') or url_for('home.index'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and employee.verify_password(
+                form.password.data):
+
+            login_user(user, form.remember_me.data)
+            flash('Logged in successfully')
+
+            if user.is_admin:
+                return redirect(url_for('home.index'))
+            else:
+                return redirect(url_for('home.index'))
+        else:
+            flash('Invalid email or password.')
+
+
+    return render_template('auth/login.html', form=form, title='Login')
 
 
 @auth.route('/logout')
