87afd86acb19f0bc900ef00ea70ffa192fc4f658
inyoka <simonmarkholland@gmail.com>
2017-02-27 11:58:45 +0700

    Mv forms to auth

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,8 +2,8 @@
 
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, ValidationError, TextField, BooleanField
-from wtforms.validators import InputRequired, Email, EqualTo, Length
-
+from wtforms.validators import InputRequired, Email, EqualTo, Length, DataRequired, Length
+from wtforms.fields import SelectMultipleField, FormField, FieldList, DateField, SelectField, TextAreaField, RadioField, IntegerField
 from ..models import User
 
 class RegistrationForm(FlaskForm):
@@ -55,4 +55,25 @@ class LoginForm(FlaskForm):
             self.username.errors.append(gettext('User not activated'))
             return False
 
+        return True
+
+
+class EditForm(FlaskForm):
+    username = StringField('User.username', validators = [DataRequired()])
+    about_me = TextAreaField('User.about_me', validators = [Length(min=0, max=140)])
+
+    def __init__(self, original_username, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.original_username = original_username
+
+    def validate(self):
+        if not Form.validate(self): #Normal flask's built in check.
+            return False
+        if self.username.data == self.original_username: #If it's just the same, no worries
+            return True
+        user = User.query.filter_by(username=self.username.data).first() #Check database now for
+        #the username guy wants
+        if user != None: #If result returns non-none, already in use.
+            self.username.errors.append('This username is already in use. Please choose another one.')
+            return False
         return True
--- app/auth/views.py
+++ app/auth/views.py
@@ -16,28 +16,27 @@ import sqlite3 as sql
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        '''
-        employee = Employee(email=form.email.data,
+        user = User(email=form.email.data,
                             username=form.username.data,
-                            fullname=form.first_name.data,
+                            fullname=form.fullname.data,
                             password=form.password.data)
 
-        '''
-        if request.method == 'GET':
-            return render_template('auth/register.html')
-        user = User(request.form['username'], request.form['fullname'],
-                    request.form['password'], request.form['email'])
-
+        # add employee to the database
         db.session.add(user)
         db.session.commit()
-        flash('User successfully registered')
+        flash('You have successfully registered! You may now login.')
+
+        # redirect to the login page
         return redirect(url_for('auth.login'))
+
     # load registration template
     return render_template('auth/register.html', form=form, title='Register')
 
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+
     form = LoginForm()
     if request.method == 'GET':
         return render_template('auth/login.html', title="login", form=form)
@@ -50,7 +49,22 @@ def login():
     login_user(registered_user, form.remember_me.data)
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('home.index'))
+    '''
 
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(user)
+        session['remember_me'] = form.remember_me.data  # Does user want remembering?
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('home.index'))
+    return render_template('auth/login.html', title='Sign-In', form=form)
+    '''
 
 @auth.route('/logout')
 def logout():
